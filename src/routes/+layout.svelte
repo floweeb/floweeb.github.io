66ca5621420192d7540e45482d719fa509e3842c
floweeb <floydqaranja@gmail.com>
2024-06-26 17:23:32 +0300

    Styling for the whole general page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,27 @@
   </script>
   
   <NavBar/>
-  <div class="bg-gray-100">
+  <div class="bg-gray-100 main-container">
     <slot />
   </div>
  <Footer/>
+
+ <style>
+  /* Global styles */
+  :global(body) {
+    background-color: #f3f4f6; /* bg-gray-100 */
+    margin: 0;
+    font-family: 'Arial', sans-serif;
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+
+  .main-container {
+    display: flex;
+    flex: 1; /* Fill remaining space */
+    justify-content: center;
+    align-items: flex-start; /* Adjust as needed */
+    padding: 1rem; /* Adjust padding for content spacing */
+  }
+</style>
