df15d29816311dbd79a9f0660ab3befe9d4f3f1d
floweeb <floydqaranja@gmail.com>
2024-06-26 15:37:08 +0300

    Some navbar styling and showing active tab

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,6 +1,17 @@
-<nav>
-    <ul>
-      <li><a href="/">Home</a></li>
-      <li><a href="/projects">Projects</a></li>
-    </ul>
-  </nav>
+<script lang="ts">
+  import { page } from '$app/stores';
+  
+  // Function to determine if a link is active
+  $: isActive = (path: string) => $page.route.id === path;
+</script>
+
+<nav class="bg-blue-500">
+  <ul class="flex w-full text-white">
+    <li class="flex-1">
+      <a href="/" class="block text-center px-4 py-2 hover:bg-blue-300 {isActive('/') ? 'bg-blue-700' : ''}">Home</a>
+    </li>
+    <li class="flex-1">
+      <a href="/projects" class="block text-center px-4 py-2 hover:bg-blue-300 {isActive('/projects') ? 'bg-blue-700' : ''}">Projects</a>
+    </li>
+  </ul>
+</nav>
