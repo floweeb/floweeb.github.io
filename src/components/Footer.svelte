25dc0aed707a2dcd30ebebb33437a04b2fc6d8c1
floweeb <floydqaranja@gmail.com>
2024-06-26 16:17:16 +0300

    Basic footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,20 @@
+<script>
+    let currentYear = new Date().getFullYear(); // Get current year dynamically
+  </script>
+  
+  <style>
+    .footer {
+      color: white;
+      text-align: center;
+      padding: 0.4rem;
+      position: fixed;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+    }
+  </style>
+  
+  <div class="footer bg-blue-500">
+    <span>&copy; Floweeb Inc. {currentYear}</span>
+  </div>
+  
