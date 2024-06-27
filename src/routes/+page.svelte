ddd9be44293d56d7070db357174604e6ffe09101
floweeb <floydqaranja@gmail.com>
2024-06-27 17:52:07 +0300

    Basic info to the homepage, just incase

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,8 @@
-<h1>HOME</h1>
+<div class="text-center">
+    <h1 class="text-center text-primary">
+        <span class="text-lg">Welcome <br> to </span>
+        <br>
+        <span class="text-2xl">FLOWEEB Inc.</span>
+    </h1>
+    <p>Still in progress</p>
+</div>
