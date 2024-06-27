4885516dd540ecce368601f5dff10b1c11d0d3e9
floweeb <floydqaranja@gmail.com>
2024-06-27 16:12:06 +0300

    slight styling changes

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,20 +1,20 @@
 <script>
     let currentYear = new Date().getFullYear(); // Get current year dynamically
-  </script>
+</script>
   
-  <style>
-    .footer {
-      color: white;
-      text-align: center;
-      padding: 0.2rem;
-      position: fixed;
-      bottom: 0;
-      left: 0;
-      width: 100%;
-    }
-  </style>
+<div class="footer bg-primary text-sm">
+  <span>&copy;Floweeb Inc. {currentYear} E-mail: floydqaranja@gmail.com</span>
+</div>
   
-  <div class="footer bg-primary">
-    <span>&copy; Floweeb Inc. {currentYear}</span>
-  </div>
+<style>
+  .footer {
+    color: white;
+    text-align: center;
+    padding: 0.2rem;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+  }
+</style>
   
