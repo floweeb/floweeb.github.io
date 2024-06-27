b481998f27a77c0bf67e9547318face483e2b2c1
floweeb <floydqaranja@gmail.com>
2024-06-27 18:44:31 +0300

    changed location in fear of not being accessible

---
+++ src/component/ProjectCard.svelte
@@ -0,0 +1,54 @@
+
+<script lang="ts">
+    // Import the Project interface
+    import type { Project } from "$lib/projects";
+  
+    // Props definition for the ProjectCard component
+    export let project: Project;
+  </script>
+  
+  <div class="card transition shadow-xl hover:shadow-lg hover:scale-95">
+    <div class="header bg-primary">
+      <h2>{project.name}</h2>
+    </div>
+    <div class="content bg-primary-light">
+      <p class="description">{project.desc}</p>
+      <a href={project.url} target="_blank" rel="noopener noreferrer" class="button bg-primary hover:bg-primary-hover">Visit Project</a>
+    </div>
+  </div>
+
+  <style lang="postcss">
+    .card {
+      border-radius: 8px;
+      overflow: hidden;
+      background-color: white;
+      margin-bottom: 20px;
+    }
+  
+    .header {
+      color: white;
+      padding: 12px;
+      font-weight: bold;
+      text-align: center;
+    }
+  
+    .content {
+      padding: 12px;
+    }
+  
+    .description {
+      margin-bottom: 10px;
+    }
+  
+    .button {
+      display: inline-block;
+      padding: 8px 16px;
+      color: white;
+      text-decoration: none;
+      border-radius: 4px;
+      cursor: pointer;
+      transition: background-color 0.3s ease;
+    }
+  
+  </style>
+  
