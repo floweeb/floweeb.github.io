d2ba2b6107a616d5220503ceb4b123a0ee784c1f
floweeb <floydqaranja@gmail.com>
2024-06-27 16:00:15 +0300

    putting in basic themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#2563EB', // This is bg-blue-600
+          light: '#eff6ff', // This is bg-blue-50
+        },
+      },
+    },
   },
   plugins: [],
 }
