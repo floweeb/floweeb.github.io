5a511d5f4d6ba91a00426be400e6db4c10602687
floweeb <floydqaranja@gmail.com>
2024-07-16 16:43:54 +0300

    Added a new project

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -8,6 +8,9 @@ export interface Project {
     { name: "Portfolio Website", url: "/",
         desc: `A website, specifically this one, to serve as a pseudo-resume
          for show casing my work.`},
+    { name: "Arcade games", url: "https://floweeb.github.io/games_demo/",
+        desc: `Working to further my skill by making simple arcade/board type games
+         , try out the games to pass time 😊, thanks. 🎉` },
     { name: "Github page", url: "https://github.com/floweeb",
         desc: `Here you can check my github page for the code of, finished
          and unfished works.` },
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
     import projects from "$lib/projects";
 	import ProjectCard from "../../component/ProjectCard.svelte";
-    // import { Project } from "$lib/projects";
 </script>
 <div>
     <h1>PROJECTS</h1>
     <ProjectCard project={projects[0]} />
     <ProjectCard project={projects[1]} />
+    <ProjectCard project={projects[2]} />
 </div>
