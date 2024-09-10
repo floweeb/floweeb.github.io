edd3e29d29c1eaf7b23078dc6cd528f6470927bf
floweeb <floydqaranja@gmail.com>
2024-09-10 20:26:42 +0300

    the fade in transition isn't working

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
     import projects from "$lib/projects";
 	import ProjectCard from "../../component/ProjectCard.svelte";
+
+    // let duration: number = 2000, default_delay: number = 300;
 </script>
 <div>
     <h1>PROJECTS</h1>
     {#each projects as project}
+        <!-- <div in:fade={{ duration, delay: default_delay }} ></div> -->
         <ProjectCard project={project}/>
     {/each}
 </div>
