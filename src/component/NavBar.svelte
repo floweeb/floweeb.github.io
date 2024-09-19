b37b7a197e4b09c50b52ee9adc9f56481dcf61fb
floweeb <floydqaranja@gmail.com>
2024-09-19 12:38:12 +0300

    Started to redesign my website with the navbar

--- src/component/NavBar.svelte
+++ src/component/NavBar.svelte
@@ -8,7 +8,7 @@
   $: isActive = (path: string) => $page.route.id === path;
 </script>
 
-<nav class="bg-primary shadow-xl h-10">
+<!-- <nav class="bg-primary shadow-xl h-10">
   <ul class="flex w-full text-white">
     <li class="flex-auto px-4 py-2 text-xl cursor-default cool-title">FLOWEEB Inc.</li>
     <li class="flex-1 ">
@@ -18,7 +18,18 @@
       <a href="/projects" class="transition-colors duration-700 block text-center px-4 py-2 {isActive('/projects') ? active_class : n_active_class}">Projects</a>
     </li>
   </ul>
+</nav> -->
+<nav class="border-b-2 h-10">
+  <ul class="flex w-full py-2">
+    <li class="flex-auto">FLOYD</li>
+    <li >
+      <a href="/">HOME</a>
+    </li>
+    <li >
+      <a href="/">ABOUT</a>
+    </li>
+    <li >
+      <a href="/projects">PROJECTS</a>
+    </li>
+  </ul>
 </nav>
-
-<style>
-</style>
